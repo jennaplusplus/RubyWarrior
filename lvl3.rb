ec5ec9dd117fa965c1ad86118d7dd5b1cba96a55
Jenna Nichols <jennaplusplus@gmail.com>
2015-11-02 15:46:25 -0800

    beginner levels 1-3

---
+++ lvl3.rb
@@ -0,0 +1,13 @@
+
+class Player
+  def play_turn(warrior)
+    if warrior.feel.empty? && warrior.health < 20
+      warrior.rest!
+    elsif warrior.feel.empty?
+      warrior.walk!
+    else
+      warrior.attack!
+    end
+  end
+end
+  
