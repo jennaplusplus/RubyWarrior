e331236e6bda0297f982d95012ea280e32a7c0b9
Jenna Nichols <jennaplusplus@gmail.com>
2015-11-03 21:46:07 -0800

    level 4

---
+++ lvl4.rb
@@ -0,0 +1,20 @@
+class Player
+  def play_turn(warrior)
+    if warrior.feel.enemy?
+       warrior.attack!
+    else
+      if warrior.health < 20 && !taking_damage?(warrior)
+        warrior.rest!
+      else
+        warrior.walk!
+      end
+    end
+    @health = warrior.health
+  end
+
+
+  def taking_damage?(warrior)
+    warrior.health < @health
+  end
+
+end
